c5e1dff058b5f86bdd1feecc1b3fa7c77c14fd0f
Yanisse <yanisseferhaoui@gmail.com>
2023-01-23 11:36:05 +0100

    import du dossier sanbox"

---
+++ sandbox/hello.c
@@ -0,0 +1,15 @@
+/* Chacun ajoute son nom ici */
+/* Auteurs : ... et ... */
+
+#include <stdio.h>
+
+int main (void) {
+	/* Oups, on a oublié le \n final. */
+	printf("Hello, world");
+
+	printf("Je calcule la réponse ... ");
+	
+	/* Oups, on s'est trompé, pour afficher un nombre, c'est
+	   plutôt %d */
+	printf("La réponse est %s\n", 42);
+}
