eeb706e26056e2e6765e3c4c7381bc4d6e685bda
Yanisse <yanisseferhaoui@gmail.com>
2023-02-17 15:31:35 +0100

    modif Numbers.py

--- Numbers.py
+++ Numbers.py
@@ -1,11 +1,10 @@
 import tensorflow as tf
+from tensorflow import keras
+from keras import layers
 
 import numpy as np
 import matplotlib.pyplot as plt
-
 import cv2
-
-print(tf.__version__)
 
 numbers = tf.keras.datasets.mnist
 (train_images, train_labels), (test_images, test_labels) = numbers.load_data()
@@ -16,31 +15,37 @@ class_names = ['Zero', 'Un', 'Deux', 'Trois', 'Quatre', 'Cinq', 'Six', 'Sept', '
 
 print(test_images.shape)
 
-# plt.figure()
-# plt.imshow(train_images[0])
-# plt.colorbar()
-# plt.grid(False)
-# plt.show()
+plt.figure()
+plt.imshow(train_images[0])
+plt.colorbar()
+plt.grid(False)
+plt.show()
 
 train_images = train_images / 255.0
-
 test_images = test_images / 255.0
 
-# plt.figure(figsize=(10,10))
-# for i in range(25):
-#     plt.subplot(5,5,i+1)
-#     plt.xticks([])
-#     plt.yticks([])
-#     plt.grid(False)
-#     plt.imshow(train_images[i], cmap=plt.cm.binary)
-#     plt.xlabel(class_names[train_labels[i]])
-# plt.show()
+plt.figure()
+plt.imshow(train_images[0])
+plt.colorbar()
+plt.grid(False)
+plt.show()
+
+
+plt.figure(figsize=(10,10))
+for i in range(25):
+    plt.subplot(5,5,i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    plt.imshow(train_images[i], cmap=plt.cm.binary)
+    plt.xlabel(class_names[train_labels[i]])
+plt.show()
 
 #Creation du model
 
 model = tf.keras.Sequential([
     tf.keras.layers.Flatten(input_shape=(28, 28)),
-    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(128, activation=tf.nn.relu),
     tf.keras.layers.Dense(10)
 ])
 
@@ -48,11 +53,35 @@ model.compile(optimizer='adam',
               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
               metrics=['accuracy'])
 
-model.fit(train_images, train_labels, epochs=10)
+history = model.fit(train_images, train_labels, validation_data=(test_images, test_labels), epochs=30, batch_size=50)
 
-test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
+(test_acc, test_loss) = model.evaluate(test_images,  test_labels, verbose=2)
 
-print('\nTest accuracy:', test_acc)
+print(history.history.keys())
+
+
+plt.figure(figsize=(16, 6))
+plt.subplot(1, 2, 1)
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_accuracy'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.grid()
+plt.legend(['train', 'test'], loc='upper left')
+plt.subplot(1, 2, 2)
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.grid()
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+
+# model.save('premier_test.h5')
+
+# print('\nTest accuracy:', test_acc)
 
 probability_model = tf.keras.Sequential([model, tf.keras.layers.Softmax()])
 
@@ -61,7 +90,6 @@ probability_model = tf.keras.Sequential([model, tf.keras.layers.Softmax()])
 # print(predictions[0])
 
 # print(np.argmax(predictions[0]))
-
 # print(test_labels[0])
 # plt.figure()
 # plt.imshow(test_images[0])
@@ -100,8 +128,8 @@ def plot_value_array(i, predictions_array, true_label):
   thisplot[predicted_label].set_color('red')
   thisplot[true_label].set_color('blue')
 
-# Plot the first X test images, their predicted labels, and the true labels.
-# Color correct predictions in blue and incorrect predictions in red.
+# # Plot the first X test images, their predicted labels, and the true labels.
+# # Color correct predictions in blue and incorrect predictions in red.
 # num_rows = 10
 # num_cols = 7
 # num_images = num_rows*num_cols
@@ -114,19 +142,19 @@ def plot_value_array(i, predictions_array, true_label):
 # plt.tight_layout()
 # plt.show()
 
-img = cv2.imread("./test/huit_2.png", 0)
+img = cv2.imread("./test/Trois.png", 0)
 img = cv2.bitwise_not(img)
+img = cv2.resize(img, (28, 28))
 img = np.expand_dims(img,0)
 
-# print(img.shape)
+
+print(img.shape)
 
 # print(test_images[0].shape)
 
-# plt.figure()
-# plt.imshow(img[0])
-# plt.colorbar()
-# plt.grid(False)
-# plt.show()
+plt.figure()
+plt.imshow(img[0])
+plt.show()
 
 
 
@@ -134,10 +162,14 @@ img = np.expand_dims(img,0)
 
 predictions_single = probability_model.predict(img)
 
-print(np.argmax(predictions_single))
+print("J'ai predit :")
+print(class_names[np.argmax(predictions_single)])
 
 
 print(predictions_single)
 plot_value_array(1, predictions_single[0], test_labels)
 _ = plt.xticks(range(10), class_names, rotation=45)
 plt.show()
+
+
+model.save('model.h5')
