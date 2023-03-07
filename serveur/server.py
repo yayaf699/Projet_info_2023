4ccb265d087785d57f9da6a418bad61b41d16711
Sofiane Chogli <sofiane.chogli@etu.univ-lyon1.fr>
2023-03-07 16:42:38 +0100

    suite tuto flask part 3 (à la moitié)

--- serveur/server.py
+++ serveur/server.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, url_for
+from flask import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '0aa3b2e238ca0390b8bebd82dd583841'
 
 posts = [
     {
@@ -27,6 +30,18 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
+
+
+@app.route("/register")
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form = form)
+
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form = form)
 
 
 if __name__ == '__main__':
